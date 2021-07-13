e56fcc52184d939e3b48d3f6318ff562274adf35
carla951 <85961931+carla951@users.noreply.github.com>
2021-07-13 21:31:02 +0300

    Add files via upload

---
+++ JenkinsExample/src/main/java/pages/HomePage.java
@@ -0,0 +1,47 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class HomePage {
+	private WebDriver driver;
+	
+	//locatori 
+	private By shopLink = By.linkText("BOOKS");
+	private By loginLink = By.linkText("Login");
+	private By eventsLink = By.linkText("EVENTS");
+	private By blogLink = By.linkText("BLOG");
+	
+	@SuppressWarnings("unused")
+	private By contactLink = By.linkText("CONTACTS");
+	
+	public By linkComingSoon = By.linkText("COMING SOON");
+	
+	public By searchIcon = By.cssSelector("button[class*='search_submit']");
+	
+	public By searchField = By.className("search_field");
+	
+	public By copyrightLink = By.linkText("KeyTraining");
+
+
+	
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+	}
+		
+	// metode specifice locatorilor
+		
+	
+	
+	
+	
+	public LoginPage navigateToLogin() {
+		driver.findElement(loginLink).click();
+		return new LoginPage(driver); }
+	
+
+}
+	
+	
+	
